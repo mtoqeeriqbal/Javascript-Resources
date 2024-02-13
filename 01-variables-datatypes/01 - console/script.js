e0c93c370ee9f2d7f13cf7a9259cc8f420e7e246
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-13 18:27:35 +0500

    Console

---
+++ 01-variables-datatypes/01 - console/script.js
@@ -0,0 +1,15 @@
+// In JavaScript, console is indeed an object. It's a predefined object provided by the JavaScript runtime environment (such as web browsers or Node.js) that exposes various methods for interacting with the console.
+
+// As an object, console has several properties and methods that developers can use for logging, debugging, and interacting with the console environment. console.log() is just one of the methods provided by the console object. Other common methods include console.error(), console.warn(), console.info(), console.table(), and more.
+
+// Here's a basic example demonstrating that console is indeed an object:
+
+// Accessing properties of the console object
+
+console.log(console);
+
+// When you execute this code, it will print the console object itself to the console, allowing you to explore its properties and methods.
+
+// In short, console is an object provided by the JavaScript environment, and console.log() is one of the methods available on this object for logging messages to the console.
+
+console.log(100);
