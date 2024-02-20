1531ce7ddeda2741ce916a91506adafa5b13b531
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-20 16:32:09 +0500

    Operators

---
+++ 01-variables-datatypes/07 - operators/script.js
@@ -0,0 +1,64 @@
+//  1. Arithmetic Operators
+
+let x;
+
+x = 5+5;
+console.log(x);
+
+x = 5 - 5;
+console.log(x);
+
+x = 5 * 5;
+console.log(x);
+
+x = 5 / 5;
+console.log(x);
+
+x = 5 % 5;
+console.log(x);
+
+
+// Concatenation
+x = 'Hello' + ' ' +'World';
+console.log(x);
+
+
+// Exponent
+x = 2 ** 3;
+console.log(x);
+
+// Increment
+x = 2;
+// x = x + 1;
+x++;
+console.log(x);
+
+// Decrement
+x = 2;
+// x = x - 1;
+x--;
+console.log(x);
+
+//  2. Assigment Operators
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+console.log(x);
+
+//  3. Comparision Operators
+
+x = 2 == '2'; // true
+x = 2 === '2'; // false - Type should also match
+
+x = 2 != '2'; // false
+x = 2 !== '2'; // true
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 >= 5;
+x = 10 <= 5;
+console.log(x);
