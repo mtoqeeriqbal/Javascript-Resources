b173cde08069c93ef48246d3781904fc2b3bcb12
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-21 19:09:14 +0500

    Numbers

---
+++ 01-variables-datatypes/10 - numbers/script.js
@@ -0,0 +1,23 @@
+let x;
+
+const num = new Number(53);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(4);
+
+x = num.toExponential(2);
+
+x = num.toLocaleString('de');
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+
+console.log(x);
