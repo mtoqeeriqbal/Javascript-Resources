54b35b151591997c352fc942b11aa109af530f66
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-21 19:28:39 +0500

    Numbers

---
+++ 01-variables-datatypes/11 - math-object/script.js
@@ -0,0 +1,25 @@
+// console.log(Math); 
+
+
+x = Math.sqrt(2);
+
+x = Math.abs(-3);
+
+x = Math.round(34.33);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.9);
+
+x = Math.pow(2,2);
+
+x = Math.min(2,4);
+
+x = Math.max(2,4);
+
+x = Math.random();
+
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
+
