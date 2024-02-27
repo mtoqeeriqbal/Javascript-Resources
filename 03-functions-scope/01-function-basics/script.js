cd37ffe49f4d9cbf6c664326b14ed79d80c47bb7
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-27 17:13:17 +0500

    function basics

---
+++ 03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,19 @@
+function sayHello(){
+    return 'Hello World!';
+}
+
+console.log(sayHello());
+
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+// }
+
+// add(1,5);
+
+
+function subtract(num1, num2){
+    return num1 - num2;
+};
+const result = subtract(20, 10);
+console.log(result);
