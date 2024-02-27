660522b81fa550962ad83b3e1fc4e6fb0d02b062
Toqeer Iqbal <toqeer.94@gmail.com>
2024-02-27 11:04:26 +0500

    array methods

--- 02-arrays-and-objects/02-array-methods/script.js
+++ 02-arrays-and-objects/02-array-methods/script.js
@@ -1,7 +1,21 @@
+let x;
+
 const arr = [12,23,4,55,66,77];
 
 arr.push(100);
-
 arr.pop();
 
-console.log(arr);
+arr.unshift(20);
+arr.shift();
+
+// arr.reverse();
+
+x = arr.includes(66);
+x = arr.indexOf(12);
+
+x = arr.slice(1,4);
+
+x = arr.splice(1, 4)
+
+
+console.log(x);
